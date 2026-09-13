44b5f9075b3bbe42e3c32532839337def386f1aa
vvennela <vishnupratikvennelakanti@gmail.com>
2026-09-13 12:36:55 -0700

    Pin FAST_START to its matching runtime helpers

--- notebooks/FAST_START.py
+++ notebooks/FAST_START.py
@@ -2,7 +2,7 @@
 # requires-python = ">=3.11,<3.14"
 # dependencies = [
 #     "marimo==0.24.2",
-#     "sera-inference[swarm,litellm] @ git+https://github.com/vvennela/weavehacks.git@f6e0ffdda2be7cb90fb3458688e4b5ffdeea9138",
+#     "sera-inference[swarm,litellm] @ git+https://github.com/vvennela/weavehacks.git@9b0c9da89e73bc729e42dfde750ca48bac5a6313",
 #     "vllm==0.26.0; sys_platform == 'linux'",
 #     "torch==2.11.0; sys_platform == 'linux'",
 #     "transformers==5.17.0",
